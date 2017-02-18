5468c35881b519fa9fc9e78c56a74523dd6f5be2
Jake Owens <Jowens@Jakes-Air-3.attlocal.net>
2017-02-18 02:45:38 -0500

    one more commit

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -12,7 +12,7 @@ var lossCount=0;
 -Random generate compPick(does this need to happen before prompt- timing?)
 -Check to make sure input is valid, convert case
 -Add to winCount/lossCount
--If winCount=1 reset lettersPicked array, restart game (display clear field on screen)
+-If winCount=1 clusysear lettersPicked array, restart game (display clear field on screen)
 -If totAllowedGuesses>=9 add to lossCount, restart game.
 -Add to lettersPicked (can we use it both as counter and display?)
 */
@@ -24,16 +24,34 @@ compPick = allLetters[Math.floor(Math.random() * allLetters.length)];
 		console.log(allLetters[i]);
 		//lettersPicked.push(youPick);
 	}
-console.log("first comppick",compPick);
+//console.log("first comppick",compPick);
 document.getElementById('guessesLeft').innerHTML = totAllowedGuesses;
+/*if(youPick !== compPick){
+	totAllowedGuesses--
 
+
+	}*/
 }
+//*document.getElementById('guessesLeft').innerHTML = totAllowedGuesses;
 document.getElementById('youPickedThis').innerHTML = lettersPicked;
-
+//document.getElementById('countYerWins').innerHTML = winCount;
+//*document.getElementById('countYerLosses').innerHTML = lossCount;*/
+//console.log(youPickedThis);
+//console.log(totAllowedGuesses);
+//console.log(lettersPicked);
 startGame();
 document.onkeyup = function(event){
 	var youPick = String.fromCharCode(event.keyCode).toLowerCase();
-	
+	//console.log("youpick", youPick);
+	//console.log("compick ",compPick)
+
+	/*if(compPick === youPick){
+		winCount++;
+		alert("YES!");
+		document.getElementById('winningLetter').innerHTML = compPick;
+		document.getElementById('countYerWins').innerHTML = winCount;
+		console.log("wincount ",winCount);
+		startGame();*/
 if(totAllowedGuesses===0){
 		lossCount++;
 		document.getElementById('countYerLosses').innerHTML = lossCount;
@@ -55,7 +73,12 @@ if(totAllowedGuesses===0){
 		document.getElementById('youPickedThis').innerHTML = lettersPicked;
 		startGame();
 
-	
+
+	/*if(totAllowedGuesses===0){
+		lossCount++;
+		document.getElementById('countYerLosses').innerHTML = lossCount;
+		alert("You are all out of Guesses!");
+		startGame();*/
 	}else if(compPick!==youPick){
 
 		totAllowedGuesses--
